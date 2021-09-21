19d9b305ed9d20075f3511d5ded7e085e092bd73
Deepak Sai Pendyala <76196490+deepaksaipendyala@users.noreply.github.com>
2021-09-21 10:09:03 +0000

    Loading data into file

--- main.py
+++ main.py
@@ -1,30 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
-import pprint
+# import pprint
+import csv
 
+fields = ['link', 'title', 'votes'] 
+filename = "data.csv"
 
 hn = []
-p=2 #p no of pages
-for i in range(p):
-  res = requests.get(f'https://news.ycombinator.com/news?p={i+1}')
-  soup = BeautifulSoup(res.text, 'html.parser')
 
-  links = soup.select('.storylink')
-  subtext = soup.select('.subtext')
 
-  def create_custom_hn(links, subtext):
-    for idx, item in enumerate(links):
-      title = item.getText()
-      href = item.get('href', None)
-      vote = subtext[idx].select('.score')
-      if len(vote):
-        points = int(vote[0].getText().replace(' points', ''))
-        if points > 99:
-          hn.append({'title': title, 'link': href, 'votes': points})
+def create_custom_hn(hn):
+    p = 2  #p no of pages
+    for i in range(p):
+        res = requests.get(f'https://news.ycombinator.com/news?p={i+1}')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        links = soup.select('.storylink')
+        subtext = soup.select('.subtext')
+        for idx, item in enumerate(links):
+            title = item.getText()
+            href = item.get('href', None)
+            vote = subtext[idx].select('.score')
+            if len(vote):
+                points = int(vote[0].getText().replace(' points', ''))
+                if points > 99:
+                    hn.append({'title': title, 'link': href, 'votes': points})
+
     return hn
-  create_custom_hn(links, subtext)
+
+
+hn = create_custom_hn(hn)
+
 
 def sort_stories_by_votes(hnlist):
-  return sorted(hnlist, key= lambda k:k['votes'], reverse=True)    
- 
-pprint.pprint(sort_stories_by_votes(hn))
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+st = sort_stories_by_votes(hn)
+# pprint.pprint(st)
+# print("done")
+# title=[]
+# link=[]
+# for i in st:
+#   title.append(i['title'])
+#   link.append(i['link'])
+
+# print(title,link)
+
+with open(filename, 'w') as csvfile: 
+    # creating a csv dict writer object 
+    writer = csv.DictWriter(csvfile, fieldnames = fields) 
+        
+    # writing headers (field names) 
+    writer.writeheader() 
+        
+    # writing data rows 
+    writer.writerows(st) 
