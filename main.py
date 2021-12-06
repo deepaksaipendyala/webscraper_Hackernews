1b8da0a44fb2d5017a6eeee8a36b6bd9185bb31d
Deepak Sai Pendyala <76196490+deepaksaipendyala@users.noreply.github.com>
2021-12-06 07:58:37 +0000

    Update cum bug fix 1.0

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ def create_custom_hn(hn):
     p = 2  #p no of pages
     for i in range(p):
         res = requests.get(f'https://news.ycombinator.com/news?p={i+1}')
+        # print(res.text)
         soup = BeautifulSoup(res.text, 'html.parser')
-        links = soup.select('.storylink')
+        links = soup.select('.titlelink')
         subtext = soup.select('.subtext')
+        # print(soup)
+        # print(links)
         for idx, item in enumerate(links):
             title = item.getText()
             href = item.get('href', None)
@@ -24,7 +27,7 @@ def create_custom_hn(hn):
                 points = int(vote[0].getText().replace(' points', ''))
                 if points > 99:
                     hn.append({'title': title, 'link': href, 'votes': points})
-
+    # print(hn)
     return hn
 
 
@@ -36,15 +39,17 @@ def sort_stories_by_votes(hnlist):
 
 
 st = sort_stories_by_votes(hn)
+# print(st)
 # pprint.pprint(st)
 # print("done")
-# title=[]
-# link=[]
-# for i in st:
-#   title.append(i['title'])
-#   link.append(i['link'])
+title=[]
+link=[]
+for i in st:
+  title.append(i['title'])
+  print("Title:",i['title'])
+  link.append(i['link'])
+  print("Link:",i['link'])
 
-# print(title,link)
 
 with open(filename, 'w') as csvfile: 
     # creating a csv dict writer object 
@@ -55,3 +60,5 @@ with open(filename, 'w') as csvfile:
         
     # writing data rows 
     writer.writerows(st) 
+
+    print("Data is stored in data.csv file")
